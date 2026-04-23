c7e337a3df7ef9d6fb953ce8e82b97f32a219c9d
extrafix <extrasensory.fix@gmail.com>
2026-04-23 17:34:07 +0300

    Procedure style

--- src/main/java/com/summer/cleaner/interpreter/CommandInterpreter.java
+++ src/main/java/com/summer/cleaner/interpreter/CommandInterpreter.java
@@ -6,7 +6,7 @@ import com.summer.cleaner.arguments.Meter;
 import com.summer.cleaner.arguments.Point;
 import com.summer.cleaner.field.Field;
 import com.summer.cleaner.out.OutMessage;
-import com.summer.cleaner.robot.Cleaner;
+import com.summer.cleaner.robot.CleanerFunctional;
 import com.summer.cleaner.robot.CleanerImpl;
 import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
@@ -21,50 +21,56 @@ public class CommandInterpreter {
       Meter.of(500),
       Meter.of(500));
 
-  Cleaner cleaner = CleanerImpl.of(
-      currentPosition,
-      currentField,
-      Angle.of(0),
-      CleanMode.WATER);
-
   InToCommandTransformer inToCommandTransformer = new InToCommandTransformer();
 
-  boolean exec(List<String> commandStrings) {
-    List<Pair<String, Object>> parsedCommandsAndArguments = inToCommandTransformer.exec(commandStrings);
 
-    // цикл по вхождениям EntryMap
-    parsedCommandsAndArguments.forEach(pair -> {
-      OutMessage outMessage = callCommand(pair);
+  boolean exec(List<String> commandStrings) {
+    List<Pair<String, Object>> parsedCommandsAndArguments = inToCommandTransformer.exec(
+        commandStrings);
+
+    CleanerImpl currentCleaner = CleanerImpl.of(
+        currentPosition,
+        currentField,
+        Angle.of(0),
+        CleanMode.WATER);
+    for (Pair<String, Object> pair : parsedCommandsAndArguments) {
+      Pair<CleanerImpl, OutMessage> updatedCleanerAndOutMessage = callCommand(currentCleaner, pair);
+      currentCleaner = updatedCleanerAndOutMessage.getLeft();
+      OutMessage outMessage = updatedCleanerAndOutMessage.getRight();
       System.out.println(outMessage.text());
-    });
+    }
+
+
     return true;
   }
 
-  private OutMessage callCommand(
+  private Pair<CleanerImpl, OutMessage> callCommand(
+      CleanerImpl cleaner,
       Pair<String, Object> pair) {
     String commandKey = pair.getKey();
     Object argument = pair.getValue();
-    return callCommand(commandKey, argument);
+    return callCommand(cleaner, commandKey, argument);
   }
 
-  private OutMessage callCommand(
+  private Pair<CleanerImpl, OutMessage> callCommand(
+      CleanerImpl cleaner,
       String commandKey,
       Object argument) {
     switch (commandKey) {
       case "move":
-        return cleaner.move((Meter) argument);
+        return CleanerFunctional.move(cleaner, (Meter) argument);
 
       case "turn":
-        return cleaner.turn((Angle) argument);
+        return CleanerFunctional.turn(cleaner, (Angle) argument);
 
       case "set":
-        return cleaner.set((CleanMode) argument);
+        return CleanerFunctional.set(cleaner, (CleanMode) argument);
 
       case "start":
-        return cleaner.start();
+        return CleanerFunctional.start(cleaner);
 
       case "stop":
-        return cleaner.stop();
+        return CleanerFunctional.stop(cleaner);
 
       default:
         throw new IllegalArgumentException("Unknown command key: " + commandKey);
