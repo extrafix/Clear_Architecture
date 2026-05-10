ef337e651a553f9adb6ac88c02a3a9f11971c69c
extrafix <extrasensory.fix@gmail.com>
2026-05-10 19:15:01 +0300

    Update OutMessage.java

--- src/main/java/com/summer/cleaner/out/OutMessage.java
+++ src/main/java/com/summer/cleaner/out/OutMessage.java
@@ -1,5 +1,31 @@
 package com.summer.cleaner.out;
+import com.summer.cleaner.dto.*;
+import java.util.Optional;
 
-public record OutMessage(String text) {
 
+public class OutMessage{
+
+private final String text;
+
+private final Optional<ValidationMessage> validationMessageOptional;
+
+public OutMessage(String text){
+  this.text=text;
+  this.validationMessageOptional = Optional.empty();
+}
+
+
+public OutMessage(String text,
+                                   ValidationMessage validationMessage){
+  this.text=text;
+  this.validationMessageOptional = Optional.of(validationMessageOptional);
+}
+
+public String text(){
+  return text;
+}
+
+public Optional<ValidationMessage> validationMessageOptional(){
+  return validationMessageOptional;
+}
 }
